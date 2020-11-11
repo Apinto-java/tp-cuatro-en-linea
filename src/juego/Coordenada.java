f982ea6fc3b095950f5a049e3f0de11b695ccb69
Apinto-java <74080365+Apinto-java@users.noreply.github.com>
2020-11-11 20:42:20 -0300

    Se crea clase coordenada

---
+++ src/juego/Coordenada.java
@@ -0,0 +1,13 @@
+package juego;
+
+public class Coordenada {
+	
+	private int columna;
+	private int fila;
+	
+	public Coordenada(int columna, int fila) {
+		
+		this.columna = columna;
+		this.fila = fila;
+	}
+}
