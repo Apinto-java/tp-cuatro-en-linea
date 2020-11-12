d60337fe0353e7bf0c8cccddaf83e1adb23580be
Apinto-java <74080365+Apinto-java@users.noreply.github.com>
2020-11-11 23:27:48 -0300

    Conteo hacia la derecha

--- src/juego/BuscadorDePatrones.java
+++ src/juego/BuscadorDePatrones.java
@@ -25,6 +25,13 @@ public class BuscadorDePatrones {
 			fichasEncontradas++;
 			columna--;
 		}
+		
+		columna = coordenadaColumna + 1;
+		
+		while(fichasEncontradas < 4 && estaDentroDeLosLimitesDelTablero(columna, coordenadaFila) && tablero[columna][coordenadaFila] == colorCasillero) {
+			fichasEncontradas++;
+			columna++;
+		}
 		/**
 		 * Busca a la izquierda de la Coordenada de la última Ficha tirada.
 		 */
