eca019dc3d807e9ac27af330fbef8a355b60aeb8
Hernán Matías Rubio <matiihr@outlook.es>
2020-11-11 21:12:57 -0300

    Se crea Clase BuscadorDePatrones.

---
+++ src/juego/BuscadorDePatrones.java
@@ -0,0 +1,31 @@
+package juego;
+
+public class BuscadorDePatrones {
+	
+	private boolean hay4EnLinea(Coordenada coordenadaUltimaFichaTirada, 
+			Casillero[][] tablero) {
+		
+		return hayPatronHorizontal() || hayPatronVertical() || 
+				hayPatronDiagonalDescendente() || hayPatronDiagonalAscendente();
+	}
+
+	private boolean hayPatronHorizontal() {
+		// TODO Apéndice de método generado automáticamente
+		return false;
+	}
+
+	private boolean hayPatronVertical() {
+		// TODO Apéndice de método generado automáticamente
+		return false;
+	}
+
+	private boolean hayPatronDiagonalDescendente() {
+		// TODO Apéndice de método generado automáticamente
+		return false;
+	}
+
+	private boolean hayPatronDiagonalAscendente() {
+		// TODO Apéndice de método generado automáticamente
+		return false;
+	}
+}
